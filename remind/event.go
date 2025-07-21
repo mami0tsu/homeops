1376267004b23e6dc6bf948eb5a9bbbb0811ea1e
mami0tsu <mami0tsu.jp@gmail.com>
2025-07-21 18:32:59 +0900

    event.goに処理を実装した

--- remind/event.go
+++ remind/event.go
@@ -8,26 +8,27 @@ import (
 type Event struct {
 	Name     string
 	Interval string    // e.g. Onetime, Weekly, Monthly, Yearly
-	Start    time.Time // e.g. 2025-01-01
-	End      time.Time // e.g. 2025-12-31
+	Start    time.Time // e.g. 2025/01/01
+	End      time.Time // e.g. 2025/12/31
 }
 
 type EventSource interface {
 	Fetch(ctx context.Context) ([]Event, error)
 }
 
-// FIXME
-// type Client struct {
-// 	source EventSource
-// }
-//
-// func NewClient(src EventSource) *Client {
-// 	return &Client{source: src}
-// }
-//
-// func (c *Client) Do(ctx context.Context) error {
-// 	events, err := c.source.Fetch(ctx)
-// 	if err != nil {
-// 	}
-// 	return nil
-// }
+type Client struct {
+	source EventSource
+}
+
+func NewClient(src EventSource) *Client {
+	return &Client{source: src}
+}
+
+func (c *Client) Do(ctx context.Context) ([]Event, error) {
+	events, err := c.source.Fetch(ctx)
+	if err != nil {
+		return err
+	}
+
+	return events, nil
+}
